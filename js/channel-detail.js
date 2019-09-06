472e8993083390cbfc4fe8290950f4999b606b72
yanwenyy <18738390058@163.com>
2019-09-06 11:09:03 +0800

    ...

--- js/channel-detail.js
+++ js/channel-detail.js
@@ -553,20 +553,7 @@ $(function(){
                     wx.onMenuShareQZone(shareData);
                     wx.onMenuShareAppMessage(shareData);
                     wx.onMenuShareTimeline(shareData);
-                    $(".scan").click(function(){
-                        wx.scanQRCode({
-                            needResult : 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-                            scanType : [ "qrCode", "barCode" ], // 可以指定扫二维码还是一维码，默认二者都有
-                            success : function(res) {
-                                var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-                                alert(result);
-                                // window.location.href = result;//因为我这边是扫描后有个链接，然后跳转到该页面
-                            },
-                            error : function(e){
-                                alert(e)
-                            }
-                        });
-                    })
+
                 });
                 wx.error(function(res){
                     console.log(res)
